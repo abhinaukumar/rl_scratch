e8b425d8c7e07173d06ca8718174dc90c1732b95
nownowk <ab.kumr98@gmail.com>
2018-06-04 13:59:43 -0400

    Added Q-Learning and Expected SARSA

--- windy_gridworld.py
+++ windy_gridworld.py
@@ -50,19 +50,82 @@ def SARSA(gridworld,eps,alpha,gamma):
             action = next_action
             step = step + 1
         steps.append(step)
-        if i%10:
+        if not i%1000:
             print "Episode length: ",steps[-1] - steps[-2]
         gridworld.reset()
         l.append(steps[-1] - steps[-2])
     return Q,steps,l
 
+def q_learn(gridworld,eps,alpha,gamma):
+    Q = np.zeros((gridworld.nrows,gridworld.ncols,gridworld.nactions))
+    step = 0
+    steps = [0]
+    l = []
+    for i in range(10000):
+        if i%10:
+            eps*=0.95
+        state = gridworld.start_state
+        while not gridworld.done:
+            e = np.random.uniform()
+            action = np.argmax(Q[state[0],state[1],:]) if e >= eps else np.random.randint(0,gridworld.nactions)
+            reward,next_state = gridworld.act(state,action)
+            Q[state[0],state[1],action] += alpha*(reward + gamma*np.argmax(Q[next_state[0],next_state[1],:]) - Q[state[0],state[1],action])
+            state = next_state.copy()
+            step = step + 1
+        steps.append(step)
+        if not i%100:
+            print "Episode length: ",steps[-1] - steps[-2], "Exploration probability: ",eps
+        gridworld.reset()
+        l.append(steps[-1] - steps[-2])
+    return Q,steps,l
+    
+def expected_SARSA(gridworld,eps,alpha,gamma):
+    Q = np.zeros((gridworld.nrows,gridworld.ncols,gridworld.nactions))
+    p = np.ones((gridworld.nactions,))*eps/gridworld.nactions    
+    step = 0
+    steps = [0]
+    l = []
+    for i in range(10000):
+        state = gridworld.start_state
+        while not gridworld.done:
+            e = np.random.uniform()
+            greedy_selection = np.argmax(Q[state[0],state[1],:])
+            action = greedy_selection if e >= eps else np.random.randint(0,gridworld.nactions)
+            reward,next_state = gridworld.act(state,action)
+            p_step = p.copy()
+            p_step[greedy_selection] += 1 - eps
+            Q[state[0],state[1],action] += alpha*(reward + gamma*np.dot(Q[next_state[0],next_state[1],:],p_step) - Q[state[0],state[1],action])
+            state = next_state.copy()
+            step = step + 1
+        steps.append(step)
+        if not i%1000:
+            print "Episode ", i, "has length: ",steps[-1] - steps[-2]
+        gridworld.reset()
+        l.append(steps[-1] - steps[-2])
+    return Q,steps,l
+    
 gridworld = WindyGridworld()
-Q,steps,l = SARSA(gridworld,0.2,0.25,1.0)
+Q,steps,l = expected_SARSA(gridworld,0.1,0.25,1.0)
 
 plt.figure()
 plt.plot(steps,np.arange(len(steps)))
+
+times = []
+for i in range(1000):
+    gridworld.reset()
+    state = gridworld.start_state
+    times.append(0)
+    while not gridworld.done:
+        action = np.argmax(Q[state[0],state[1],:])
+        state = gridworld.act(state,action)[-1]
+        times[-1]+=1
+print "Average time taken:",np.mean(times)
+
+gridworld.reset()
+Q,steps,l = SARSA(gridworld,0.1,0.25,1.0)
+
 plt.figure()
-plt.plot(l)
+plt.plot(steps,np.arange(len(steps)))
 
 times = []
 for i in range(1000):
