8876185e8d956a5ec517d334edaa90cb83e6f085
nownowk <ab.kumr98@gmail.com>
2018-06-24 00:46:55 -0400

    Fixed loss and now it's not training

--- argmax_actor_critic.py
+++ argmax_actor_critic.py
@@ -13,6 +13,7 @@ import torch
 from torch import nn
 from torch.distributions import Categorical
 from torch import optim
+import torch.nn.functional as F
 
 import argparse
 from itertools import count
@@ -47,12 +48,14 @@ def softmax(x):
 #        return F.softmax(action_scores, dim=-1), state_values
 
 class ActorCritic(nn.Module):
-    def __init__(self,input_dim,dims,n_actions):
+    def __init__(self,input_dim,dims):
         super(ActorCritic,self).__init__()
         self.input_dim = input_dim
-        self.n_actions = n_actions
+        #self.n_scales = 10
+        #self.n_thresholds = 1
+        self.n_actions = 10 #self.n_scales * self.n_thresholds
         self.layer1 = nn.Linear(input_dim,dims)
-        self.action_layer = nn.Linear(dims,n_actions)
+        self.action_layer = nn.Linear(dims,self.n_actions)
         self.value_layer = nn.Linear(dims,1)
         self.saved_log_probs = []
         self.rewards = []
@@ -118,12 +121,12 @@ class Agent():
     def __init__(self,env):
         self.n_options = env.n_options
         self.eps_env = env.eps
-        self.n_actions = 10
-        self.model = ActorCritic(self.n_options,25,self.n_actions).to(devices[self.eps_env])
+        self.model = ActorCritic(self.n_options,25).to(devices[self.eps_env])
         self.channels = np.zeros((self.n_options,))
-        self.scales = np.arange(1.0,11.0)
-        np.random.shuffle(self.scales)
+        #self.scales = np.arange(1.0,11.0)
+        #np.random.shuffle(self.scales)
         self.decay = 1.0
+        self.losses = []
         #self.action_values = np.ones(self.n_actions,)
         #self.action_values = np.random.normal(1.0,0.25,(self.n_actions,))
                 
@@ -133,15 +136,21 @@ class Agent():
     def act(self,greedy=False):
         state = torch.from_numpy(self.channels).float().unsqueeze(0).to(devices[self.eps_env])
         probs,value = self.model.forward(state)
-#        print probs
+        #print probs
         if not greedy:
+            #print probs
             dist = Categorical(probs)
             a = dist.sample()
             self.model.saved_log_probs.append(dist.log_prob(a))
             self.model.values.append(value)
-            return a.item()
+            a = a.item()
         else:
-            return torch.argmax(probs).item()
+            a = torch.argmax(probs).item()
+            
+        scale = 1 + a
+        #print scale
+        #threshold = (10 - self.model.n_thresholds + a%self.model.n_thresholds)/10.0
+        return scale#,threshold
         
     def clear_memory(self):
         del self.model.saved_log_probs[:]
@@ -149,26 +158,50 @@ class Agent():
             
 def finish_episode(agent,optimizer):
     #policy_loss = []
-    rewards = []
-    
-    #for r in agent.policy.rewards[::-1]:
-    #    R = r + R
-    #    rewards.insert(0,R)
-    rewards = np.cumsum(agent.model.rewards)
-    rewards = torch.Tensor(rewards).to(devices[agent.eps_env])
+#    rewards = []
+#    R = 0
+#    for r in agent.model.rewards[::-1]:
+#        R = r + R
+#        rewards.insert(0,R)
+    #print agent.model.rewards[::-1],type(agent.model.rewards[::-1])
+    #print np.cumsum(agent.model.rewards[::-1])[::-1]
+    rewards = np.cumsum(agent.model.rewards[::-1])[::-1]
+    rewards = torch.Tensor(rewards.copy()).to(devices[agent.eps_env])
+    #std = 0 if rewards.shape == torch.Size([1]) else rewards.std()
+    #rewards = (rewards - rewards.mean()) / (std + np_eps)
+    #print rewards
     values = torch.squeeze(torch.cat(agent.model.values))
-#    std = 0 if rewards.shape == torch.Size([1]) else rewards.std()
+    #std = 0 if rewards.shape == torch.Size([1]) else rewards.std()
         
 #    rewards = (rewards - rewards.mean()) / (std + np_eps)
 
 #    for log_prob,reward in zip(agent.model.saved_log_probs,rewards):
 #        policy_loss.append(-log_prob*reward)
+#    R = 0
+#    blah_rewards = []
+#    policy_losses = []
+#    value_losses = []
+#    for r in agent.model.rewards[::-1]:
+#        R = r + R
+#        blah_rewards.insert(0, R)
+#    blah_rewards = torch.tensor(blah_rewards).to(devices[agent.eps_env])
+#    #blah_rewards = (blah_rewards - blah_rewards.mean()) / (blah_rewards.std() + np_eps)
+#    for log_prob, value, r in zip(agent.model.saved_log_probs, agent.model.values, blah_rewards):
+#        reward = r - value.item()
+#        #print reward
+#        policy_losses.append(-log_prob * reward)
+#        value_losses.append(F.smooth_l1_loss(value, torch.tensor([[r]]).to(devices[agent.eps_env])))
+#    optimizer.zero_grad()
+#    blah_loss = torch.stack(policy_losses).sum() + torch.stack(value_losses).sum()
 
     optimizer.zero_grad()
-    loss = torch.sum(-torch.cat(agent.model.saved_log_probs)*(rewards - values)) + nn.modules.loss.SmoothL1Loss()(values,rewards)
+    loss = torch.sum(-torch.cat(agent.model.saved_log_probs)*(rewards - values)) + nn.modules.loss.SmoothL1Loss(size_average=False)(values,rewards)
+#    if loss != blah_loss:
+#        print loss,blah_loss
     loss.backward()
     optimizer.step()
     
+    agent.losses.append(loss.item())
     del agent.model.rewards[:]
     del agent.model.saved_log_probs[:]
     del agent.model.values[:]
@@ -190,7 +223,7 @@ def train(args):
     #p = []
     #scales = []
         
-    for i in range(100):
+    for i in range(250):
             
         # Report the performance of the greedy policy every 50 iterations
         
@@ -200,11 +233,13 @@ def train(args):
         agent.reset_channels()
         correct = 0
         length = 0
-        while env.iters < 1000:
+        while env.iters < 500:
             length+=1
-            choice = agent.act(greedy=True)
-            agent.channels = agent.decay*agent.channels + agent.scales[choice]*evidence
+            scale = agent.act(greedy=True)
+            #print scale,threshold
+            agent.channels = agent.decay*agent.channels + scale*evidence
             prob = softmax(agent.channels)
+            #print prob
             guess = np.argmax(prob) if np.max(prob) > 0.5 else None
             reward,evidence,done = env.step(guess)
             tot_reward+=reward
@@ -216,9 +251,9 @@ def train(args):
         if not i%10:
             print "Iteration {} in environment with eps {}".format(i,env.eps)
             
-        corrects.append(correct/10.0)
-        lengths.append(length/1000.0)
-        rewards.append(tot_reward/1000.0)
+        corrects.append(correct/5.0)
+        lengths.append(length/500.0)
+        rewards.append(tot_reward/500.0)
             #scales.append(agent.scales[np.argmax(agent.action_values)])
         agent.clear_memory()
         
@@ -230,8 +265,8 @@ def train(args):
         while env.iters < 250:
             #e = np.random.uniform()
             #choice = np.argmax(agent.action_values) if e >= eps else np.random.randint(0,agent.n_actions)
-            choice = agent.act()
-            agent.channels = agent.decay*agent.channels + agent.scales[choice]*evidence # The scale chosen is a measure of the agent's confidence in the decision
+            scale = agent.act()
+            agent.channels = agent.decay*agent.channels + scale*evidence # The scale chosen is a measure of the agent's confidence in the decision
             prob = softmax(agent.channels)
             guess = np.argmax(prob) if np.max(prob) > 0.5 else None
             reward,evidence,done = env.step(guess)
@@ -246,10 +281,10 @@ def train(args):
     agent.reset_channels()
     correct = 0
     length = 0
-    while env.iters < 1000:
+    while env.iters < 500:
         length+=1
-        choice = agent.act(greedy=True)
-        agent.channels = agent.decay*agent.channels + agent.scales[choice]*evidence
+        scale = agent.act(greedy=True)
+        agent.channels = agent.decay*agent.channels + scale*evidence
         prob = softmax(agent.channels)
         guess = np.argmax(prob) if np.max(prob) > 0.5 else None
         reward,evidence,done = env.step(guess)
@@ -259,9 +294,9 @@ def train(args):
             agent.reset_channels()
             correct += int(reward != -30)
         
-    corrects.append(correct/10.0)
-    lengths.append(length/1000.0)
-    rewards.append(tot_reward/1000.0)
+    corrects.append(correct/5.0)
+    lengths.append(length/500.0)
+    rewards.append(tot_reward/500.0)
     #scales.append(agent.scales[np.argmax(agent.action_values)])
     agent.clear_memory()
     
@@ -280,7 +315,7 @@ if __name__ == '__main__':
     #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     if not args.test:
         if args.eps != None:
-            ret = train((args.eps,2e-4))
+            ret = train((args.eps,1e-3))
             plt.figure()
             plt.title('Delay vs time')
             plt.plot(ret[0],label="eps = %.2f" % (args.eps))
@@ -290,14 +325,18 @@ if __name__ == '__main__':
             plt.figure()
             plt.title('Reward vs time')
             plt.plot(ret[2],label="eps = %.2f" % (args.eps))
+            plt.figure()
+            plt.title('Loss vs time')
+            plt.plot(ret[3].losses[::250],label="eps = %.2f" % (args.eps))
             
         else:
             p = Pool(10)
-            ret = p.map(train,zip(np.arange(10)/10.0,np.ones((10,))*2e-4))
+            ret = p.map(train,zip(np.arange(10)/10.0,np.ones((10,))*1e-2))
             
             lengths = [r[0] for r in ret]
             corrects = [r[1] for r in ret]
             rewards = [r[2] for r in ret]
+            losses = [r[3].losses for r in ret]
 #            scales = [r[3] for r in ret]
 #            q = [softmax(np.array(r[-1]),axis=-1).T for r in ret]
             
@@ -317,6 +356,12 @@ if __name__ == '__main__':
             plt.title("Reward vs Time")
             for i in range(len(rewards)):
                 plt.plot(rewards[i],label="eps = %.2f" % (i/10.0))
+            plt.legend()
+            
+            plt.figure()
+            plt.title("Loss vs Time")
+            for i in range(len(rewards)):
+                plt.plot(losses[i][::250],label="eps = %.2f" % (i/10.0))
             plt.legend()
             
 #            fig,axes = plt.subplots(len(q),sharex=True)
